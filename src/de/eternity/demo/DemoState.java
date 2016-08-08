ef2e0a9907b4a3ca60c78c794c347ada48220c2b
Julian Baehr <julian.baehr@googlemail.com>
2016-08-08 10:35:35 +0200

    added 2000 new random textures that are drawn

--- src/de/eternity/demo/DemoState.java
+++ src/de/eternity/demo/DemoState.java
@@ -1,5 +1,7 @@
 package de.eternity.demo;
 
+import java.util.Random;
+
 import de.eternity.IGameState;
 import de.eternity.gfx.IRenderQueue;
 import de.eternity.gfx.Texture;
@@ -15,6 +17,18 @@ public class DemoState implements IGameState {
 	Texture t1 = new Texture(100, 100, 0xff<<16);//full red
 	Texture t2 = new Texture(100, 100, 0xff<<8);//full green
 	
+	Texture[] textures = new Texture[2000];
+	int[] xs = new int[textures.length], ys = new int[xs.length];
+	{
+		Random rand = new Random();
+		for(int i = 0; i < xs.length; i++){
+			textures[i] = new Texture(10, 10, rand.nextInt());
+			xs[i] = rand.nextInt(380);
+			ys[i] = rand.nextInt(280);
+		}
+		
+	}
+	
 	double x = 0, speed = 100;
 	
 	@Override
@@ -22,15 +36,20 @@ public class DemoState implements IGameState {
 
 		//update x
 		x += speed * delta;
-		if(x >= 299)x = 0;
+		if(x >= 319)x = 0;
 	}
 
 	@Override
 	public void applyRenderContext(IRenderQueue renderQueue) {
-		
+
 		//render rectangles
 		renderQueue.addEntity(t1, x, 100);
 		renderQueue.addEntity(t2, 200, 0);
+		
+		for(int i = 0; i < xs.length; i++){
+			renderQueue.addEntity(textures[i], xs[i], ys[i]);
+		}
+		
 	}
 
 	@Override
