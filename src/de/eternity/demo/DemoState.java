bc001b7b3b9d84d8ae20377fb3959b636c24be4a
Julian Baehr <julian.baehr@googlemail.com>
2016-08-12 12:50:39 +0200

    changed demo state

--- src/de/eternity/demo/DemoState.java
+++ src/de/eternity/demo/DemoState.java
@@ -1,10 +1,15 @@
 package de.eternity.demo;
 
+import java.io.FileNotFoundException;
 import java.util.Random;
+
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 
 import de.eternity.IGameState;
 import de.eternity.gfx.IRenderQueue;
 import de.eternity.gfx.Texture;
+import de.eternity.map.Map;
 
 /**
  * Draws a default background with a red and a green rectangle. One rectangle is moving sideways.
@@ -18,7 +23,23 @@ public class DemoState implements IGameState {
 	Texture t2 = new Texture(100, 100, 0xff<<8);//full green
 	{t1.foo();t2.foo();}
 	
-	Texture[] textures = new Texture[1000];
+	Map m;
+	{
+		try {
+			m = Map.readMap("res/t.json");
+		} catch (JsonSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonIOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	Texture[] textures = new Texture[100];
 	int[] xs = new int[textures.length], ys = new int[xs.length];
 	{
 		Random rand = new Random();
@@ -43,22 +64,25 @@ public class DemoState implements IGameState {
 	@Override
 	public void applyRenderContext(IRenderQueue renderQueue) {
 
-		//render rectangles
+		renderQueue.getCamera().set(10, 10);
+		//m.renderMap(renderQueue);
+		
+//		//render rectangles
 		renderQueue.addBackground(t1, x, x);
-		renderQueue.addBackground(t1, 300-x, x);
-		renderQueue.addBackground(t1, x, 200-x);
-		renderQueue.addBackground(t1, 300-x, 200-x);
-		
-		renderQueue.addBackground(t1, x, 100);
-
-		renderQueue.addBackground(t1, 300-x, 100);
-		
-		renderQueue.addBackground(t1, 150, x);
-		renderQueue.addBackground(t1, 150, 200-x);
-		renderQueue.addBackground(t2, 150, 100);
+//		renderQueue.addBackground(t1, 300-x, x);
+//		renderQueue.addBackground(t1, x, 200-x);
+//		renderQueue.addBackground(t1, 300-x, 200-x);
+//		
+//		renderQueue.addBackground(t1, x, 100);
+//
+//		renderQueue.addBackground(t1, 300-x, 100);
+//		
+//		renderQueue.addBackground(t1, 150, x);
+//		renderQueue.addBackground(t1, 150, 200-x);
+//		renderQueue.addBackground(t2, 150, 100);
 		
 		for(int i = 0; i < xs.length; i++){
-			renderQueue.addEntity(textures[i], xs[i], ys[i]);
+			//renderQueue.addEntity(textures[i], xs[i], ys[i]);
 		}
 	}
 
