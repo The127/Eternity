c0f21b9e225a629673598a12d446b34b73f7771e
Julian Baehr <julian.baehr@googlemail.com>
2016-08-15 12:45:29 +0200

    Used IRenderQueue instead of RenderQueue

--- src/de/eternity/map/Tile.java
+++ src/de/eternity/map/Tile.java
@@ -1,7 +1,7 @@
 package de.eternity.map;
 
 import de.eternity.gfx.Animation;
-import de.eternity.gfx.RenderQueue;
+import de.eternity.gfx.IRenderQueue;
 import de.eternity.gfx.TextureStorage;
 
 public class Tile {
@@ -16,7 +16,7 @@ public class Tile {
 		animation.update(delta);
 	}
 	
-	public void applyRenderContext(RenderQueue renderQueue, TextureStorage textureStorage, double x, double y){
+	public void applyRenderContext(IRenderQueue renderQueue, TextureStorage textureStorage, double x, double y){
 		renderQueue.addBackground(animation.getCurrentTexture(textureStorage), x, y);
 	}
 }
