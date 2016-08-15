e7f9cf0661a80a4cd9758a9db9762f71d7b9d323
Julian Baehr <julian.baehr@googlemail.com>
2016-08-15 12:46:48 +0200

    Finished the tile storage

--- src/de/eternity/map/TileStorage.java
+++ src/de/eternity/map/TileStorage.java
@@ -9,4 +9,24 @@ public class TileStorage {
 	public void addTile(Tile tile, int index){
 		tiles.add(tile);
 	}
+	
+	/**
+	 * Updates all tiles.
+	 * @param delta
+	 */
+	public void update(double delta){
+		
+		int size = tiles.size();
+		for(int i = 0; i < size; i++){
+			
+			Tile tile = tiles.get(i);
+			if(tile != null){
+				tile.update(delta);
+			}
+		}
+	}
+	
+	public Tile get(int index){
+		return tiles.get(index);
+	}
 }
