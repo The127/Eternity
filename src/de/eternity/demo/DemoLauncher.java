960e541be43ccceec2899c0cd125e2869491b007
Julian Baehr <julian.baehr@googlemail.com>
2016-08-21 14:35:07 +0200

    added a lua game state

--- src/de/eternity/demo/DemoLauncher.java
+++ src/de/eternity/demo/DemoLauncher.java
@@ -1,7 +1,9 @@
 package de.eternity.demo;
 
+import java.awt.KeyboardFocusManager;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.IOException;
 
 import de.eternity.Game;
 import de.eternity.GameData;
@@ -12,21 +14,28 @@ import de.eternity.gui.Display;
 import de.eternity.gui.DisplayMode;
 import de.eternity.input.ButtonInput;
 import de.eternity.input.KeyboardAdapter;
+import de.eternity.support.lua.EngineLuaEnvironment;
 
 public class DemoLauncher {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		//init input
 		ButtonInput keyboard = new ButtonInput(256);
+		
+		//directly apply keyboard adapter to the keyboard focus manager
+		//this way not every component must have a key listener
 		KeyboardAdapter keyboardAdapter = new KeyboardAdapter(keyboard);
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyboardAdapter);
+		
+		//init game data
+		GameData gameData = new GameData(keyboard);
 		
 		//init display
-		DisplayMode displayMode = new DisplayMode(800, 600, 400, 300);
+		DisplayMode displayMode = gameData.getDisplayMode();
 		Display display = new Display("Demo", displayMode);
 		
 		//set listeners
-		display.addKeyListener(keyboardAdapter);
 		display.addWindowListener(new WindowListener() {
 			
 			//ignore all these
@@ -50,11 +59,57 @@ public class DemoLauncher {
 		Renderer renderer = new Renderer(new Texture(display.getCanvas()), camera);
 		
 		//init game
-		GameData gameData = new GameData(32, keyboard);
 		
 		Game game = new Game(gameData, renderer, display::refreshScreen);
+		EngineLuaEnvironment engineLuaEnvironment = new EngineLuaEnvironment(game);
 		
 		game.pushGameState(new DemoState());
+		game.pushGameState(engineLuaEnvironment.loadGameState(
+				   "local luaGameState = {}\n\r"
+				
+				+  "local does_remain_on_stack = true\n\r"
+				
+				+  "local function startup()\n\r"
+				+  "	print('startup lua')\n\r"
+				+  "end\n\r"
+				
+				+  "local function shutdown()\n\r"
+				+  "	print('shutdown lua')\n\r"
+				+  "end\n\r"
+
+				+  "local function pause()\n\r"
+				+  "	print('pause lua')\n\r"
+				+  "end\n\r"
+
+				+  "local function unpause()\n\r"
+				+  "	print('unpause lua')\n\r"
+				+  "end\n\r"
+				
+				+  "local function update(delta)\n\r"
+//				+  "	print('update lua')\n\r"
+				+  "	poll_keyboard()\r\n"
+				+  "	if is_key_pressed(VK_A) then\r\n"
+				+  "		pop_game_state()\r\n"
+				+  "	end\r\n"
+				+  "end\n\r"
+
+				+  "local function apply_render_context(renderQueue)\n\r"
+//				+  "	print('render lua')\n\r"
+				+  "end\n\r"
+
+				+  "luaGameState.does_remain_on_stack = does_remain_on_stack\n\r"
+				
+				+  "luaGameState.startup = startup\n\r"
+				+  "luaGameState.shutdown = shutdown\n\r"
+				
+				+  "luaGameState.pause = pause\n\r"
+				+  "luaGameState.unpause = unpause\n\r"
+				
+				+  "luaGameState.update = update\n\r"
+				+  "luaGameState.apply_render_context = apply_render_context\n\r"
+
+				+  "return luaGameState\n\r"
+		));
 		
 		//start the game
 		game.start();
