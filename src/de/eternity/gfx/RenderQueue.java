bf4d6c0b20c317e4020fe7659eb29d632d3b7712
Julian Baehr <julian.baehr@googlemail.com>
2016-08-21 22:28:40 +0200

    fixed serious flashing bug - List.sort sorts the whole list - changed from list to array - this way it is possible to only soirt a specific part of the array

--- src/de/eternity/gfx/RenderQueue.java
+++ src/de/eternity/gfx/RenderQueue.java
@@ -1,14 +1,13 @@
 package de.eternity.gfx;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class RenderQueue implements IRenderQueue {
 	
 	private Camera camera;
 	
-	//ArrayList has best list.get(index) time
-	private List<RenderQueueEntry> entries = new ArrayList<>();
+	//use array
+	private RenderQueueEntry[] entries = {new RenderQueueEntry()};//new array with 1 entry
 	private int size = 0;
 	
 	public RenderQueue(Camera camera) {
@@ -36,13 +35,13 @@ public class RenderQueue implements IRenderQueue {
 	}
 	
 	private RenderQueueEntry addEntry(Texture texture, double x, double y){
-		
+
 		//request new RenderQueueEntry
-		if(entries.size() < size +1)
-			entries.add(new RenderQueueEntry());
+		if(entries.length == size)
+			enlargeArray();
 		
 		//reuse old RenderQueueEntry
-		RenderQueueEntry entry = entries.get(size);
+		RenderQueueEntry entry = entries[size];
 		
 		if(!entry.setValues(texture, x, y, camera.getCameraArea()))//if not on screen
 			return null;
@@ -53,18 +52,26 @@ public class RenderQueue implements IRenderQueue {
 		return entry;
 	}
 	
+	private void enlargeArray(){
+		RenderQueueEntry[] temp = entries;
+		entries = new RenderQueueEntry[temp.length*2];
+		for(int i = 0; i < temp.length; i++)
+			entries[i] = temp[i];
+		for(int i = temp.length; i < entries.length; i++)
+			entries[i] = new RenderQueueEntry();
+	}
+	
 	public RenderQueueEntry get(int index){
-		return entries.get(index);
+		return entries[index];
 	}
 	
 	public void sort(){
-		
-		entries.sort((e1, e2) ->{
+		Arrays.sort(entries, 0, size, (e1, e2) ->{
 			return e1.getDepth() - e2.getDepth();
 		});
 	}
-
-	public void reset() {
+	
+	void reset() {
 		//reset current size for next iteration
 		size = 0;
 	}
