9ea2c14251ea970a2e39c5927968e2bee8a59491
Julian Baehr <julian.baehr@googlemail.com>
2016-08-21 14:35:18 +0200

    additional resource files

---
+++ src/de/eternity/GameSettings.java
@@ -0,0 +1,14 @@
+package de.eternity;
+
+import de.eternity.gui.DisplayMode;
+
+public class GameSettings {
+
+	int tilesize;
+	
+	String modsPath;
+	String mapsPath;
+	String tilesetsPath;
+	
+	DisplayMode displayMode;
+}
