feddafe54a0bae482f59c729c35d1374e896becd
Julian Baehr <julian.baehr@googlemail.com>
2016-08-21 14:34:55 +0200

    extended functionality

--- src/de/eternity/GameData.java
+++ src/de/eternity/GameData.java
@@ -1,8 +1,16 @@
 package de.eternity;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.moandjiezana.toml.Toml;
+
 import de.eternity.gfx.TextureStorage;
+import de.eternity.gui.DisplayMode;
 import de.eternity.input.ButtonInput;
+import de.eternity.map.GameMap;
 import de.eternity.map.TileStorage;
+import de.eternity.util.TextureLoader;
 
 /**
  * Holds all the global information for the game.
@@ -19,10 +27,21 @@ public class GameData {
 	private TextureStorage textureStorage = new TextureStorage();
 	private TileStorage tileStorage = new TileStorage();
 	
+	private GameSettings gameSettings;
+	
 	//TODO: create from a file
-	public GameData(int tileSize, ButtonInput keyboard){
-		this.tileSize = tileSize;
+	public GameData(ButtonInput keyboard) throws IOException {
+		
+		gameSettings = new Toml().read(new File("res/settings.toml")).to(GameSettings.class);
+		
+		this.tileSize = gameSettings.tilesize;
 		this.keyboard = keyboard;
+		
+		TextureLoader.loadTextures(gameSettings.tilesetsPath, textureStorage);
+	}
+	
+	public DisplayMode getDisplayMode(){
+		return gameSettings.displayMode;
 	}
 	
 	public ButtonInput getKeyboard(){
@@ -35,5 +54,9 @@ public class GameData {
 	
 	public TileStorage getTileStorage(){
 		return tileStorage;
+	}
+	
+	public GameMap getGameMap(String mapName){
+		return null;
 	}
 }
