fb7c3fecd2d55cccbc4d981ae187dd2b60ea4043
Julian Baehr <julian.baehr@googlemail.com>
2016-08-21 22:25:15 +0200

    changed demo state (input)

--- res/gameStates/lua_test_state.lua
+++ res/gameStates/lua_test_state.lua
@@ -3,10 +3,16 @@ local luaGameState = {}
 local does_remain_on_stack = true
 
 local map
+local tex
+
+local x, y, speed
+
+speed = 10
 
 local function startup()
 	print('startup lua')
 	map = get_game_map('test')
+	tex = get_game_data():getTextureStorage():getTexture(400)
 end
 
 local function shutdown()
@@ -24,8 +30,21 @@ end
 local function update(delta)
 
 	poll_keyboard()
+	
+	x = 0
+	y = 0
+	
 	if is_key_pressed(VK_A) then
-		pop_game_state()
+		x = 100 * delta
+	end
+	if is_key_pressed(VK_D) then
+		x = -100 * delta
+	end
+	if is_key_pressed(VK_W) then
+		y = 100 * delta
+	end
+	if is_key_pressed(VK_S) then
+		y = -100 * delta
 	end
 	
 	update_tile_animations(delta)
@@ -34,8 +53,10 @@ end
 
 local function apply_render_context(renderQueue)
 
+	renderQueue:getCamera():move(x, y)
+
 	map:renderMap(renderQueue, get_game_data())
-	
+	renderQueue:addEntity(tex, 100, 100)
 end
 
 luaGameState.does_remain_on_stack = does_remain_on_stack
