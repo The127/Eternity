453a3f84905abfeb635b0ee25a7214bce0a61bc8
Julian Baehr <julian.baehr@googlemail.com>
2016-08-31 12:51:52 +0200

    finished text rendering

--- src/de/eternity/gfx/Text.java
+++ src/de/eternity/gfx/Text.java
@@ -18,25 +18,23 @@ public class Text extends Texture{
 	
 	private int[] buffer2;
 	
-	private int maxChars = 0;
-	
-	private boolean hasChanged = true;
+	private boolean hasChanged = false;
 	
 	public Text(int horizontalChars, int verticalChars, int textColor, GameData gameData) {
-		super(horizontalChars*8, (verticalChars == 1 ? 10 : verticalChars*11 - 1)/*include spacing between lines*/);
+		super(horizontalChars*gameData.getSettings().getTextWidth(), 
+				(verticalChars == 1 ? gameData.getSettings().getTextHeight() : (verticalChars*(gameData.getSettings().getTextHeight() +1) - 1))
+				/*include spacing between lines*/);
 
 		this.textColor = textColor & 0x00FFFFFF;//get rid of alpha
 		this.gameData = gameData;
 		
-		maxChars = horizontalChars*verticalChars;
 		//safe buffer
 		buffer2 = buffer;
 	}
 	
 	public Text(String oneLine, int textColor, GameData gameData){
 		this(oneLine.length(), 1, textColor, gameData);
-		text = oneLine;
-		
+		setText(oneLine);
 		update();
 	}
 	
@@ -60,19 +58,75 @@ public class Text extends Texture{
 			
 			char[] chars = text.toCharArray();
 			
-			int width = 0, height = 0;
-			for(int i = 0; i < chars.length && i < maxChars; i++){
-				Texture character = gameData.getTextureStorage().getTexture(chars[i] - 32);
+			int drawWidth = 0, drawHeight = 0;
+			int cWidth = gameData.getSettings().getTextWidth();
+			int cHeight = gameData.getSettings().getTextHeight();
+			
+			drawStringLoop : for(int i = 0; i < chars.length; i++){
 				
-				int cWidth = character.getWidth();
-				int cHeight = character.getHeight();
+				char c = chars[i];
+				
+				//non drawable character
+				if(c < 32 || c > 126){
+					if(c == '\n'){
+						//no more space downwards
+						if(drawHeight == this.height)
+							break drawStringLoop;
 
-				for(int y = 0; y < cHeight; y++)
-					for(int x = 0; x < cWidth; x++)
-						buffer[x + width + (y) * this.width] = 
-								textColor | character.buffer[x + y * cWidth];
+						//new line
+						drawHeight += 1 + cHeight;
+						drawWidth = 0;
+					}else if(c == '\t'){
 
-				width += cWidth;
+						System.out.println("tab");
+						//a tab is at most 3 white spaces
+						int tab =  3 - (drawWidth / cWidth) % 3;
+						System.out.println(tab);
+						
+						tabLoop : for(int n = 0; n < tab; n++){
+							drawWidth += cWidth;
+							System.out.println("tab");
+							
+							//new line?
+							if(drawWidth == this.width){
+								drawHeight += 1 + cHeight;
+								drawWidth = 0;
+								
+								//still space?
+								if(drawHeight >= this.height)
+									break drawStringLoop;
+								
+								//if new line in tab then stop tab
+								break tabLoop;
+							}
+						}
+					}
+				}else{
+					//draw char
+					System.out.println(drawHeight/cHeight + "/" + drawWidth/cWidth);
+					Texture charTex = gameData.getTextureStorage().getTexture(chars[i] - 32);
+					for(int y = 0; y < cHeight; y++)
+						for(int x = 0; x < cWidth; x++)
+							buffer[x + drawWidth + (y + drawHeight) * width] = 
+									textColor | charTex.buffer[x + y * cWidth];
+					
+					drawWidth += cWidth;
+					
+					//new line?
+					if(drawWidth == this.width){
+						System.out.println("new 2");
+						drawHeight += 1 + cHeight;
+						drawWidth = 0;
+						
+						//still space?
+						if(drawHeight >= this.height)
+							break drawStringLoop;
+					}
+				}
+				
+				//hit end of text area
+				if(drawWidth == this.width && drawHeight == this.height)
+					break drawStringLoop;
 			}
 		}
 	}
